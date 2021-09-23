b9fea3c67fc52562b01d61a1248bab9480989170
Портфолио <80357121+Islam1595@users.noreply.github.com>
2021-09-23 20:04:27 +0300

    Хуки и React Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route } from 'react-router-dom';
+import logosvg from './assets/img/pizza-logo.svg';
+
+import { Header } from './components';
+import { Home, Cart } from './pages';
+
+console.log(logosvg);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <Route path="/" component={Home} exact />
+        <Route path="/cart" component={Cart} exact />
+      </div>
     </div>
   );
 }
